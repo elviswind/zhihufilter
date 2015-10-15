3c87bf1adffbb19c57e89fe9a589233d30e2d138
peter@ef <peter@ef.com>
2015-10-15 10:33:07 +0800

    init files

---
+++ background.js
@@ -0,0 +1,22 @@
+function filter() {
+    var x = document.querySelectorAll('.questions .item');
+    for (var i = 0; i < x.length; i++) {
+        var y = x[i].querySelector('.zm-item-vote-count');
+        if (y) {
+            var vote = parseInt(y.innerHTML);
+            if (vote < 30) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+        else {
+            x[i].parentNode.removeChild(x[i]);
+        }
+    }
+}
+
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.executeScript({
+        code: "(" + filter.toString() + ")()"
+    });
+});
